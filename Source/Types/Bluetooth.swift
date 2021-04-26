5312eb04a20a58c7edb8def2e658a08422943a07
Alin Radut <alin.claudiu.radut@gmail.com>
2021-04-26 08:53:09 +0300

    Update bluetooth permissions to use iOS13 API where available

--- Source/Types/Bluetooth.swift
+++ Source/Types/Bluetooth.swift
@@ -33,11 +33,22 @@ let BluetoothManager = CBPeripheralManager(
 
 extension Permission {
     var statusBluetooth: PermissionStatus {
-        switch CBPeripheralManager.authorizationStatus() {
-        case .restricted:                 return .disabled
-        case .denied:                     return .denied
-        case .notDetermined, .authorized: break
-        @unknown default:                 return .notDetermined
+
+        if #available(iOS 13.1, *) {
+            switch CBManager.authorization {
+            case .restricted:                 return .disabled
+            case .denied:                     return .denied
+            case .notDetermined:              break
+            case .allowedAlways:              break
+            @unknown default:                 return .notDetermined
+            }
+        } else {
+            switch CBPeripheralManager.authorizationStatus() {
+            case .restricted:                 return .disabled
+            case .denied:                     return .denied
+            case .notDetermined, .authorized: break
+            @unknown default:                 return .notDetermined
+            }
         }
 
         guard Defaults.stateBluetoothManagerDetermined else { return .notDetermined }
